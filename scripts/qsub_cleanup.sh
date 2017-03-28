0c1c1dfb1fc4301976b3e02f1301eea178f83bb3
bogemad <daniel.bogema@dpi.nsw.gov.au>
2017-03-29 10:05:14 +1100

    Fixed bug in cleanup scripts to remove items not associated with read files

--- scripts/qsub_cleanup.sh
+++ scripts/qsub_cleanup.sh
@@ -19,3 +19,12 @@ parallel -j 4 'tar xvzf {}' ::: `ls *.tar.gz`
 rm -rf *.tar.gz core* RAxML* total.snp*
 cd ..
 rm -rf excluded_sequences
+
+for dir in `ls analysis_results`; do
+if `ls raw_data/reads/${dir}* &> /dev/null`; then
+echo "$dir has a read file and will be kept"
+else
+echo "$dir has no read file and will to be deleted"
+rm -rf analysis_results/$dir
+fi
+done
